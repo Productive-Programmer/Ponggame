7c52cdfb08f700b119faf39f180ad99bbcf61c3a
ASADKHAN <asadkhan2001.pathan@gmail.com>
2020-04-15 17:48:40 +0530

    This is the complete product

---
+++ pong.py
@@ -0,0 +1,121 @@
+import turtle
+
+wn = turtle.Screen()
+score_a = 0
+score_b = 0
+wn.title("Pong by asad")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+
+# paddle B
+
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.penup()
+paddle_b.goto(350, 0)
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+# ball
+
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 0.2
+ball.dy = 0.2
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Playee A: {}   PlayerB:{}".format(score_a, score_b), align="center",
+          font=("Courier", 24, "normal"))
+
+
+# Functions
+
+
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y1 = paddle_b.ycor()
+    y1 += 20
+    paddle_b.sety(y1)
+
+
+def paddle_b_down():
+    y1 = paddle_b.ycor()
+    y1 -= 20
+    paddle_b.sety(y1)
+
+
+# Key Binndings
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+# MainLoop
+while True:
+    wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor()+ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+    if ball.xcor() > 390:
+        ball.setx(390)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Playee A: {}   PlayerB:{}".format(score_a, score_b), align="center",
+                  font=("Courier", 24, "normal"))
+    if ball.xcor() < -390:
+        ball.setx(-390)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Playee A: {}   PlayerB:{}".format(score_a, score_b), align="center",
+                  font=("Courier", 24, "normal"))
+    # Paddle and ball collision
+
+    if ball.xcor() > 340 and ball.xcor() < 350 and (ball.ycor() < paddle_b.ycor()+40 and ball.ycor() > paddle_b.ycor()-40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    elif ball.xcor() < -340 and ball.xcor() > -350 and (ball.ycor() < paddle_a.ycor()+40 and ball.ycor() > paddle_b.ycor()-40):
+        ball.setx(-340)
+        ball.dx *= -1
